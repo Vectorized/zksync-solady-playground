cce0f4888f7de51313fc8d75da2a19bafbf4fdea
Vectorized <webby1111@hotmail.com>
2024-12-27 18:29:53 +0000

    Squash

---
+++ test/ZKsyncERC1967Factory.t.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import "../src/ZKsyncERC1967Factory.sol";
+
+contract SampleImplementation {
+    uint256 public x;
+
+    bytes public constant NAME = "Implementation";
+
+    event Foo();
+
+    error Hehe();
+
+    function foo() public {
+        emit Foo();
+    }
+
+    function setX(uint256 newX) public {
+        x = newX;
+    }
+
+    function hehe() public pure {
+        revert Hehe();
+    }
+
+    function version() public pure virtual returns (uint256) {
+        return 1;
+    }
+}
+
+contract SampleImplementation2 is SampleImplementation {
+    function version() public pure virtual override returns (uint256) {
+        return 2;
+    }
+}
+
+contract ZKsyncERC1967FactoryTest is Test {
+    ZKsyncERC1967Factory public factory;
+    address public implementation;
+    address public implementation2;
+
+    event LogBytes32(bytes32 x);
+    event LogBytes(bytes x);
+
+    function setUp() public {
+        factory = new ZKsyncERC1967Factory();
+        implementation = address(new SampleImplementation());
+        implementation2 = address(new SampleImplementation2());
+    }
+
+    function testDeployDeterministicAndUpgrade() public {
+        bytes32 salt = 0x0000000000000000000000000000000000000000ff112233445566778899aabb;
+        address predicted = factory.predictDeterministicAddress(factory.PROXY_HASH(), salt);
+        assertEq(factory.implementationOf(predicted), address(0));
+        address instance = factory.deployProxyDeterministic(implementation, address(this), salt);
+        assertEq(factory.implementationOf(predicted), implementation);
+        assertEq(predicted, instance);
+        SampleImplementation(instance).setX(123);
+        assertEq(SampleImplementation(instance).x(), 123);
+        assertEq(SampleImplementation(instance).version(), 1);
+        assertGt(instance.code.length, 0);
+
+        factory.upgradeAndCall(
+            instance, implementation2, abi.encodeWithSignature("setX(uint256)", uint256(456))
+        );
+        assertEq(SampleImplementation(instance).x(), 456);
+        assertEq(SampleImplementation(instance).version(), 2);
+    }
+
+    function testDeployBeaconProxyDeterministicAndUpgrade() public {
+        bytes32 salt = 0x0000000000000000000000000000000000000000ff112233445566778899aabb;
+        address predicted = factory.predictDeterministicAddress(factory.BEACON_HASH(), salt);
+        assertEq(factory.implementationOf(predicted), address(0));
+        address beacon = factory.deployBeaconDeterministic(implementation, address(this), salt);
+        assertEq(ZKsyncUpgradeableBeacon(beacon).implementation(), implementation);
+        assertEq(factory.implementationOf(predicted), implementation);
+        assertEq(predicted, beacon);
+
+        predicted = factory.predictDeterministicAddress(factory.BEACON_PROXY_HASH(), salt);
+        address beaconProxy = factory.deployBeaconProxyDeterministic(beacon, salt);
+        assertEq(predicted, beaconProxy);
+        assertEq(factory.implementationOf(beaconProxy), implementation);
+
+        SampleImplementation(beaconProxy).setX(123);
+        assertEq(SampleImplementation(beaconProxy).x(), 123);
+        assertEq(SampleImplementation(beaconProxy).version(), 1);
+
+        factory.upgrade(beacon, implementation2);
+        assertEq(SampleImplementation(beaconProxy).version(), 2);
+    }
+}
